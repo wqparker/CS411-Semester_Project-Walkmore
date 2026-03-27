1c4a831e7f4346fffdf3caa1a93a90800021bbd7
Ohchul Shin <ocshin@bu.edu>
2026-03-26 20:19:46 -0400

    Added export

--- server/Graph/GraphNode.js
+++ server/Graph/GraphNode.js
@@ -1,4 +1,6 @@
-class Node {
+import { Location } from './Location.js';
+
+export class Node {
     //Node class to represent Bus / Subway stops
     constructor(id, Location) {
         this.id = id;          //MTA uses ID as a key for stations
