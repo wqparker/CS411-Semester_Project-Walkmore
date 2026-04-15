62b12ca4e00ff6cd4156e449bc7c6b0b9142620c
Ohchul Shin <ocshin@bu.edu>
2026-04-15 14:33:11 -0400

    Adding input handler

---
+++ server/ValidateInput.js
@@ -0,0 +1,88 @@
+import {getAddressFromCoord, getAddressFromName} from './algorithm/APICaller.js';
+async function checkDestination(input) {
+  /** The function is expected to return 4 fields. 
+      The result as a code:
+        0 : success, meainng that the destination is in boundary(NYC)
+        1 : fail, the location exists, but the destination is out of boundary  
+        2 : fail, the location does not exist
+      Its latitude, longitude, and formatted address.
+  **/
+  const coordRegex = /^\s*(-?\d+(?:\.\d+)?)\s*,\s*(-?\d+(?:\.\d+)?)\s*$/;
+  const match = input.match(coordRegex);
+  if (match) {
+    /** If the input matches format as coordinates **/
+    const result = await getAddressFromCoord(input); 
+    const lat = match[1];
+    const lon = match[2];
+    if (result == null){
+        return {
+            code: 2, // does not exist
+            lat: null,
+            lon: null,
+            address: null
+        };
+    }
+    const isNYC = result.address_components.some(c => 
+      (c.types.includes("locality") || c.types.includes("sublocality")) && 
+      ["New York", "Brooklyn", "Bronx", "Queens", "Staten Island"].includes(c.long_name)
+      );
+    if(isNYC){
+         console.log(`Address for coordinates: "${input}":`, result?.formatted_address);
+         console.log(`lat: "${lat}" lon: "${lon}"`);
+        return {
+            code: 0, // Inside NYC
+            lat: result.geometry.location.lat,
+            lon: result.geometry.location.lon,
+            address: result.formatted_address
+        };
+    }
+    else{
+        console.log("Outside NYC");
+        return {
+            code: 1, // outside NYC
+            lat: result.geometry.location.lat,
+            lon: result.geometry.location.lon,
+            address: result.formatted_address
+        };
+    }
+  } else {
+    /** If the input is name or address **/
+      const result = await getAddressFromName(input);
+      if(result == null){
+        return {
+            code: 2, // Does not exist
+            lat: null,
+            lon: null,
+            address: null
+        }
+      }
+      const lat = result.location.latitude;
+      const lon = result.location.longitude;
+      const isNYC = result.addressComponents.some(c => 
+      (c.types.includes("locality") || c.types.includes("sublocality")) && 
+      ["New York", "Brooklyn", "Bronx", "Queens", "Staten Island"].includes(c.longText)
+      );
+      if(isNYC){
+        console.log(`In NYC`);
+        return {
+            code: 0, // Inside NYC
+            lat: lat,
+            lon: lon,
+            address: result.formatted_address
+        }    
+      } else{
+        console.log(`Out of NYC`);
+        return {
+            code: 1, // Outside NYC
+            lat: lat,
+            lon: lon,
+            address: result.formatted_address
+        }
+      }
+  }
+}
+
+export{checkDestination};
+//checkDestination("Empire State Building");
+// checkDestination("350 5th Ave, New York, NY 10118");
+//checkDestination("40.7484405,-73.9856643");
